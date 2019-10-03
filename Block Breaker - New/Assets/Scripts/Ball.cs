72c974904df6e7434df55ff2d69b7dc03e4e9ac1
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-10-03 09:10:51 +0200

    Moving the Paddle with Mouse and Ball
    Moving the Paddle with Mouse and Ball

---
+++ Block Breaker - New/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //we put a Paddle object in the Ball since they will be connected
+    [SerializeField] Paddle myPaddle;
+
+    Vector2 paddleToBallDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //this will save the Distance between the Ball and the Paddle
+       paddleToBallDistance = this.transform.position - myPaddle.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockBallToPaddle();
+    }
+
+    //this method is done to refactor the code.
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(myPaddle.transform.position.x, myPaddle.transform.position.y);
+        //set the Ball's position to the Paddle's position + the distance between them
+        transform.position = paddlePosition + paddleToBallDistance;
+    }
+
+
+}
--- Block Breaker - New/Assets/Scripts/Paddle.cs
+++ Block Breaker - New/Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
     // Start is called before the first frame update
     void Start()
@@ -15,8 +17,11 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        //Mathf.Clamp: clamps the value of mousePosition between
+        //minX and maxX
+        float limitMousePosition = Mathf.Clamp(mousePosition, minX, maxX);
+        Vector2 paddlePosition = new Vector2(limitMousePosition, transform.position.y);
         
-        Vector2 paddlePosition = new Vector2(mousePosition, transform.position.y);
         transform.position = paddlePosition;
     }
 }
