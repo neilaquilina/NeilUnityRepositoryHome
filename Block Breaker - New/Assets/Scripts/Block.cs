6aebec9adf8d87f8d5ac908893433f8a6e8a1024
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-10-14 13:07:41 +0200

    Created Blocks and Start Menu

--- Block Breaker - New/Assets/Scripts/Block.cs
+++ Block Breaker - New/Assets/Scripts/Block.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+      Destroy(this.gameObject);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
